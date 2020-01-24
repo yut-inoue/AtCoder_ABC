45f01d1658fb1d0cdc6b5f6e89722d2dd89b3eca
Yutaro Inoue <yinoue.1996787@gmail.com>
2020-01-24 17:27:51 +0900

    2020/1/24

--- ABC125/ABC125_C.py
+++ ABC125/ABC125_C.py
@@ -2,29 +2,20 @@ n=int(input())
 al=list(map(int,input().split()))
 
 import fractions
+ans=0
+left=[0]
+for i in range(1,n):
+    ans=fractions.gcd(ans,al[i-1])
+    left.append(ans)
+#print(left)
+right=[0 for i in range(n+1)]
+for i in range(1,n+1):
+    right[n-i]=fractions.gcd(right[n-i+1],al[n-i])
+#print(right)
 
-def gcdoflist(l):
-    n=len(l)
-    if n==1:
-        return l[0]
-    else:
-        ans=l[0]
-        for i in range(1,n):
-            ans=fractions.gcd(ans,l[i])
-        return ans
-m_list=[]
-#端点の場合
-#右端
-m_list.append(gcdoflist(al[0:n-1]))
+ans=0
 
-#左端
-m_list.append(gcdoflist(al[1:n]))
-ans=max(m_list)
-for i in range(1,n-1):
-    left=al[0:i]
-    right=al[i+1:n]
-    temp=fractions.gcd(gcdoflist(left),gcdoflist(right))
-    if temp>ans:
-        ans=temp
+for i in range(n):
+    ans=max(ans,(fractions.gcd(left[i],right[i+1])))
 print(ans)
 
