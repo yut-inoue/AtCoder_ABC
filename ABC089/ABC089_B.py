d99dcc5414f269e96c15210aba864049083814e7
Yutaro Inoue <yinoue.1996787@gmail.com>
2020-07-07 21:45:06 +0900

    2020/7/7

---
+++ ABC089/ABC089_B.py
@@ -0,0 +1,9 @@
+n=int(input())
+#a,b=map(int,input().split())
+sl=list(map(str,input().split()))
+#l=[list(map(int,input().split())) for i in range(n)]
+
+if 'Y' in sl:
+    print("Four")
+else:
+    print("Three")
--- ABC173/ABC173_C.py
+++ ABC173/ABC173_C.py
@@ -1,4 +1,42 @@
 #n=int(input())
-#a,b=map(int,input().split())
+import copy
+h,w,K=map(int,input().split())
 #l=list(map(int,input().split()))
-#l=[list(map(int,input().split())) for i in range(n)]
+l=[list(input()) for i in range(h)]
+rowl=[]
+coll=[]
+
+def count_black(l2d):
+    res=0
+    for l in l2d:
+        res+=l.count('#')
+    return res
+
+for i in range(2**h):
+    temp=[]
+    for k in range(h):
+        if ((i >> k) & 1):#二進数iの下から数えてj桁目が1か否か
+            temp.append(k)
+    rowl.append(temp)
+for j in range(2**w):
+    temp=[]
+    for k in range(w):
+        if ((j >> k) & 1):
+            temp.append(k)
+    coll.append(temp)
+
+count=0
+# 全探索
+rowall=["r" for i in range(w)]
+for row in rowl:
+    for col in coll:
+        test=copy.deepcopy(l)
+        for i in row:
+            test[i]=rowall
+        for j in col:
+            for i in range(h):
+                test[i][j]='r'
+        if count_black(test)==K:
+            count+=1
+
+print(count)
